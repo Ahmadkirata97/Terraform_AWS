fcf01912afb488fe7ef8cc0de265ac88f54a3a29
Ahmad12333 <ahmad.aldon@outlook.com>
2023-07-14 22:31:49 +0300

    Create SNS Topic

---
+++ SNS/SNS.tf
@@ -0,0 +1,23 @@
+resource "aws_sns_topic" "terraform_topic" {
+  name = "config-topic"
+  # Specify a delivery policy when server-side error occures 
+  delivery_policy = <<EOF
+  {
+    "healthyRetryPolicy": {
+        "minDelayTarget": 1,
+        "maxDelayTarget": 60,
+        "numRetries": 50,
+        "numNoDelayRetries": 3,
+        "numMinDelayRetries": 2,
+        "numMaxDelayRetries": 35,
+        "backoffFunction": "exponential"
+    },
+    "throttlePolicy": {
+        "maxReceivesPerSecond": 10
+    },
+    "requestPolicy": {
+        "headerContentType": "application/json"
+    }
+}
+EOF
+}
