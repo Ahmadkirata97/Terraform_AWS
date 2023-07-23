254080f91178b99bc31f35f6feb0cf25762fbc03
Ahmad12333 <ahmad.aldon@outlook.com>
2023-07-23 20:37:14 +0300

    add policy resource

--- SNS/SQS.tf
+++ SNS/SQS.tf
@@ -12,19 +12,25 @@ resource "aws_sqs_queue" "terraform_queue" {
   receive_wait_time_seconds = 10
 
   # Policy that allows anyone to send messages to the queue
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.terraform_queue.arn}"
-    }
-  ]
+  
 }
-POLICY
+
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = aws_sqs_queue.terraform_queue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:GetQueueUrl"
+        ]
+        Resource = aws_sqs_queue.terraform_queue.arn
+      }
+    ]
+  })
 }
